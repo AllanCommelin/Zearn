f8f9bbb8a22b7cca161a6d9e1ee1834deeee52fc
BenjaminDuez <benjduez@gmail.com>
2020-06-01 17:00:00 +0200

    update middleware

--- app/Http/Middleware/ProfessorMiddleware.php
+++ app/Http/Middleware/ProfessorMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class ProfessorMiddleware
 {
@@ -15,7 +16,7 @@ class ProfessorMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user()->role != 'professor')
+        if (Auth::user()->role != 'professor')
         {
             abort(401);
         }
--- app/Http/Middleware/StudentMiddleware.php
+++ app/Http/Middleware/StudentMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class StudentMiddleware
 {
@@ -15,7 +16,7 @@ class StudentMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user()->role != 'student')
+        if (Auth::user()->role != 'student')
         {
             abort(401);
         }
